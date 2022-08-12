6fb308a6df0421ec39ffe186e7ef3364df6c8c59
lucas <lucasabdala94@gmail.com>
2022-08-12 12:47:04 -0300

    header responsive

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import { Login } from "../views/auth/Login/Login";
-import { Register } from "../views/auth/Register/Register";
 import BasicLayout from "../components/BasicLayout/BasicLayout";
 
 export default function Home() {
   return (
       <BasicLayout>
             <div className="home">  
-              
+
                   <h1>contenido pagina</h1>
                   <h1>contenido pagina</h1>
                   <h1>contenido pagina</h1>
