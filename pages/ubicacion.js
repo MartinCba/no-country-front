1dc335c885c5743d39e7852b2a52babf4653f8a5
lucas <lucasabdala94@gmail.com>
2022-08-18 08:46:08 -0300

    agregando ubicacion

--- pages/ubicacion.js
+++ pages/ubicacion.js
@@ -1,45 +1,45 @@
-import BasicLayout from "../components/BasicLayout/BasicLayout";
-import { Icon } from "semantic-ui-react";
-import Map from "../api/map";
+import React,{Fragment} from 'react';
+import BasicLayout from '../components/BasicLayout/BasicLayout';
+import { Icon } from 'semantic-ui-react';
 
 export default function ubicacion() {
   return (
-    <BasicLayout>
-      <h1 className="title">Ubicación</h1>
-      <main className="contenedor">
-        <section className="contenedor__secction">
-          <h2>Gimnasio </h2>
-          <div className="contenedor__secction--ubicacion">
-            <Icon name="thumbtack" />
-            <div>
-              <h3>Buenos Aires - CABA</h3>
-              <p>Av. Córdoba 5932</p>
+      <BasicLayout>
+        <h1 className="title">Ubicación</h1>
+        <main className="contenedor">
+          <section className="contenedor__secction">
+            <h2>Gimnasio </h2>
+            <div className="contenedor__secction--ubicacion">
+              <Icon name="thumbtack" />
+              <div>
+                <h3>Buenos Aires - CABA</h3>
+                <p>Av. Córdoba 5932</p>
+              </div>
+              <MapExample />
             </div>
-          </div>
-          <Map />
-        </section>
-        <section className="contenedor__secction">
-          <h2>Horarios </h2>
-          <h3>Dias Normales</h3>
-          <div>
-            <p>
-              <span>Lunes a Viernes:</span> 08:00 hs a 21:00 hs.
-            </p>
-            <p>
-              <span>Sabado:</span> 08:00 hs a 18:00 hs.
-            </p>
-          </div>
-          <h3>Feriados</h3>
-          <div>
-            <p>
-              <span>Lunes a Viernes:</span> 08:00 hs a 13:00 hs.
-            </p>
-            <p>
-              <span>Sabado:</span> Cerrado
-            </p>
-          </div>
-        </section>
-      </main>
-    </BasicLayout>
+          </section>
+          <section className="contenedor__secction">
+            <h2>Horarios </h2>
+            <h3>Dias Normales</h3>
+            <div>
+              <p>
+                <span>Lunes a Viernes:</span> 08:00 hs a 21:00 hs.
+              </p>
+              <p>
+                <span>Sabado:</span> 08:00 hs a 18:00 hs.
+              </p>
+            </div>
+            <h3>Feriados</h3>
+            <div>
+              <p>
+                <span>Lunes a Viernes:</span> 08:00 hs a 13:00 hs.
+              </p>
+              <p>
+                <span>Sabado:</span> Cerrado
+              </p>
+            </div>
+          </section>
+        </main>
+      </BasicLayout>
   );
 }
