2cb02ade44ce9f1631e17f6edf89562d69bbcb06
Lucasabdala94 <lucasabdala94@gmail.com>
2022-08-26 20:53:08 +0000

    cambiado colores

---
+++ pages/sinprejuicios.js
@@ -0,0 +1,12 @@
+import BasicLayout from "../components/BasicLayout/BasicLayout";
+
+export default function sinprejuicios() {
+    return (
+      <BasicLayout>
+        <div>
+          <h1>sin prejuicios</h1>
+        </div>
+      </BasicLayout>
+    );
+  }
+  
