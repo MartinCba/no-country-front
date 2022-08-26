34d57e1ca5053ad48b3012b844d712af2b382b7f
Lucasabdala94 <lucasabdala94@gmail.com>
2022-08-26 13:06:11 +0000

    terminadom login y logout

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React,{useMemo,useEffect,useState} from 'react';
+import { useRouter } from 'next/router';
 
 import "../scss/global.scss";
 import 'semantic-ui-css/semantic.min.css';
@@ -10,7 +11,7 @@ import jwtDecode from 'jwt-decode';
 import AuthContext from "../context/AuthContext";
 
 //importamos la funcion que almacena token en localStorage.
-import { setToken,getToken } from '../api/token';
+import { setToken,getToken,removeToken } from '../api/token';
 
 
 // packete para mostrar mensajes en consola.
@@ -20,11 +21,17 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function MyApp({ Component, pageProps }) {
 
   const [auth,setAuth]=useState(undefined);
+  //Nos ayuda a ejecutar el useEffect que actualiza el contexto general.
+  const [realoadUser,setRealoadUser]=useState(false);
+  //Extraemos el router de nex/router
+  const router = useRouter();
 
+  // Obtiene el token y si existe setea el token en nuestro stado global. Tiene implementado su recarga.
   useEffect(() => {
     
     const token= getToken();
     if(token){
+      //setea un contexto general.
       setAuth({
         token,
         idUser: jwtDecode(token).id,
@@ -33,8 +40,8 @@ export default function MyApp({ Component, pageProps }) {
     }else{
       setAuth(null);
     }
-  
-  },[]);
+    setRealoadUser(false)
+  },[realoadUser]);
 
   const login=(token)=>{
     //setea el token en el localstorage.
@@ -46,14 +53,23 @@ export default function MyApp({ Component, pageProps }) {
       nombre: jwtDecode(token).nombre,
     });
   }
+  // funcion de logout.
+  const logout=()=>{
+    if(auth){
+      removeToken();
+      setAuth(null); // Borra datos del usuario del context general.
+      router.push('/');
+    }
+  }
 
-
+  
+  // Almacena los valores de auth apra evitar que la pagina recarge constantemente toda la aplicacion.
   const authData= useMemo(
     () => ({
       auth,
       login,
-      logout: ()=>null,
-      setRelaodUser: ()=>null
+      logout,
+      setRealoadUser,
     }),[auth]
   );
 
