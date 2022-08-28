8c04dd8f07a6f5c5be71172485d44016d58e1d1e
Lucasabdala94 <lucasabdala94@gmail.com>
2022-08-28 20:37:08 +0000

    agregado usuario al menu

--- pages/_app.js
+++ pages/_app.js
@@ -34,7 +34,6 @@ export default function MyApp({ Component, pageProps }) {
       setAuth({
         token,
         idUser: jwtDecode(token).id,
-        nombre: jwtDecode(token).nombre,
       });
     }else{
       setAuth(null);
