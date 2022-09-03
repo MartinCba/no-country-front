1492ece6696630d0f316c87bb6e31479746affca
catepecu49 <lucasabdala49@gmail.com>
2022-09-03 18:15:18 +0000

    solucion

--- views/auth/Register/Register2.js
+++ views/auth/Register/Register2.js
@@ -6,7 +6,7 @@ import * as Yup from "yup";
 import { registerApi } from '../../../api/Auth/user';
 import { getAllActivity } from "../../../api/actividades";
 import { getMeApi } from '../../../api/Auth/user';
-import UseAuth from '../../../hooks/useAuth';
+import UseAuth from '../../../hooks/UseAuth';
 
 export default function Register2(props) {
   const [loading, setLoading] = useState(false);
